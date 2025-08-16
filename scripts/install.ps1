c1c62961f1eafdddc507814c079a948e2a428583
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-08-16 11:46:25 -0300

    feat: add files and flow launcher

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,3 +1,6 @@
+# Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 winget install Mozilla.Firefox.DeveloperEdition
 
 winget install Microsoft.Teams
@@ -11,7 +14,8 @@ winget install Git.Git
 
 winget install Microsoft.dotnet
 
-# Tools
+# Utilities for fuzzy find
+# ripgrep, bat and fzf are required for FindItFaster VSCode extension
 winget install BurntSushi.ripgrep.MSVC
 winget install sharkdp.bat
 winget install fzf
@@ -29,6 +33,12 @@ winget install Obsidian.Obsidian
 winget install Rem0o.FanControl
 winget install 7zip.7zip
 winget install Spotify.Spotify
+
+# Files applications
+winget install -e --id FilesCommunity.Files
+
+# Better launcher
+winget install "Flow Launcher"
 
 # Python version manager
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"; Remove-Item "./install-pyenv-win.ps1"
